ed4b1f8db4d2419d2386e1466674afcae9f47563
alex0n0 <42526086+alex0n0@users.noreply.github.com>
2020-08-28 17:50:52 +1000

    fix routing issues

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from "./app-store/routes/home/home.component";
-import { ShopDashboardComponent } from "./app-store/routes/shop-dashboard/shop-dashboard.component";
-import { OfficeDashboardComponent } from './app-office/routes/office-dashboard/office-dashboard.component';
+// import { ShopDashboardComponent } from "./app-store/routes/shop-dashboard/shop-dashboard.component";
+// import { OfficeDashboardComponent } from './app-office/routes/office-dashboard/office-dashboard.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
-  { path: "dashboard", component: ShopDashboardComponent },
-  { path: "secret", component: OfficeDashboardComponent }
+  // { path: "dashboard", component: ShopDashboardComponent },
+  // { path: "secret", component: OfficeDashboardComponent }
 ];
 
 @NgModule({
